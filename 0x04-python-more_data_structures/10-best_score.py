df6021de7cc97b26d470092c903e56b17479befa
YassineNabat <nabatyassine1@gmail.com>
2023-12-06 16:44:33 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None or my_dict == {}:
+    if not a_dictionary:
         return None
-    big = max(my_dict.values())
-    for key, value in my_dict.items():
-        if value is big
-            return key
+    return max(a_dictionary, key=a_dictionary.get)
