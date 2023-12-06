600289adb622ab7b26edf73d962e548bd8728fc4
YassineNabat <nabatyassine1@gmail.com>
2023-12-06 16:52:11 +0100

    list mult

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if my_list and len(my_list):
-        nbr = 0
-        d = 0
-        for tup in my_list:
-            nbr += (tup[0] * tup[1])
-            d += tup[1]
-        return (nbr / d)
-    return 0
+    return (list(map((lambda x: x * number), my_list)))
