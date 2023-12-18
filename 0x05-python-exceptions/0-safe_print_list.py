c0e69e01ec1dae2c8b48e1c1048acb80b23a000c
YassineNabat <nabatyassine1@gmail.com>
2023-12-18 17:53:37 +0100

    list print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except(ValueError, TypeError):
-        return False
-    else:
-        return True
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for j in range(x):
+        try:
+            print(my_list[m], end="")
+            i += 1
+        except IndexError:
+            break
+    print()
+    return i
