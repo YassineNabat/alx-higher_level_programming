344d6232c48b2c7b2f9bfc67de93f7e83aafb623
YassineNabat <nabatyassine1@gmail.com>
2023-12-20 15:47:00 +0100

    updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,7 +7,9 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
-def __init__(self, size):
+    
+
+    def __init__(self, size):
         """Initializes a square
         Args:
             size (int): size of a side of the square
