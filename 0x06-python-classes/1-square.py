cb57503227d3962f68d9c8532c71d10ca361e7c2
YassineNabat <nabatyassine1@gmail.com>
2023-12-20 15:45:12 +0100

    updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Square class definition"""
 
+
 class Square:
     """Represents a square
     Attributes:
         __size (int): size of a side of the square
     """
-    def __init__(self, size):
+
+
+def __init__(self, size):
         """Initializes a square
         Args:
             size (int): size of a side of the square
