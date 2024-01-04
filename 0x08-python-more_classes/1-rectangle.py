cd4eece8e61b61ad6b3befa7ffd3f090d736aef8
YassineNabat <nabatyassine1@gmail.com>
2024-01-04 14:54:00 +0100

    class rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,12 +19,6 @@ class Rectangle:
         """
         return self.__width
 
-    @property
-    def height(self):
-        """height
-        """
-        return self.__height
-
     @width.setter
     def width(self, value):
         """ width setter
@@ -35,6 +29,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
     
+    @property
+    def height(self):
+        """height
+        """
+        return self.__height
+
     @height.setter
     def height(self, value):
         """ height setter
