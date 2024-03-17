c71509447405617712311fe9ff1978697c4bdd32
YassineNabat <nabatyassine1@gmail.com>
2024-03-17 19:20:10 +0000

    max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
+
+    max = my_list[0]
+    for i in range(1, len(my_list)):
+        if max < my_list[i]:
+            max = my_list[i]
+        else:
+            continue
+            
+    return (max)
